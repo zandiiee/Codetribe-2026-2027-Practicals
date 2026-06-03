dcce29b2364ba6e5c54b96c244f5c9aead0b5d59
Zandiiee <zandilezulu13@gmail.com>
2026-06-03 13:44:46 +0200

    more added

--- PA0601.js
+++ PA0601.js
@@ -1,5 +1,6 @@
 let student = {
     age : 23 ,
-    name : "Zandile"
-    course: "English Literature"
+    name : "Zandile",
+    course: "English Literature",
+    student id : "000055577766688"
 }
