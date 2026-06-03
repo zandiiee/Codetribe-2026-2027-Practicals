76578fff113c2df1f30b5fa00f858bb5de4f2d7c
Zandiiee <zandilezulu13@gmail.com>
2026-06-03 14:11:22 +0200

    PA0602-Arrays

---
+++ PA0602.js
@@ -0,0 +1,19 @@
+const students =[{
+firstName: "Zandile",
+age : 23,
+course : "English Literature"
+},
+
+{
+firstName: "Lee-shaan",
+age : 20,
+course: "Marketing"
+},
+
+{
+firstName: "Leigh -Rae",
+age : 21,
+course : "Design"
+}
+];
+console.log(students)
