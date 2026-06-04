64b84f42d0530ecbbe49dacd972c7f0195c6a534
Zandiiee <zandilezulu13@gmail.com>
2026-06-04 13:11:16 +0200

    PA0901

---
+++ PA0901.js
@@ -0,0 +1,13 @@
+
+function finalMark(mark){
+
+    if( mark <= 50 ){
+        score = "fail";
+    }else ( mark >= 50 )
+    {
+        score = "pass";
+    };
+    console.log(score)
+}
+
+finalMark(45)
