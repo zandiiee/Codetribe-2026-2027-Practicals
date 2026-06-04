7db65f64151452bdbf520a89db3c4e8ee5685471
Zandiiee <zandilezulu13@gmail.com>
2026-06-04 09:47:14 +0200

    PA0801-Understand-Function

---
+++ PA0801.js
@@ -0,0 +1,11 @@
+/*  ATM WITHDRAWAL SYSTEM
+
+STEP 1 - Create a function called withdrawal
+STEP 2 - Make sure the function can be called by multiple users 
+STEP 3 - Create a for loop that decremnents the amount by a specific nummber
+STEP 4 - With a return statement make sure the displayed output is the new amount
+
+*/
+ function withdrawal(){
+    
+ }
