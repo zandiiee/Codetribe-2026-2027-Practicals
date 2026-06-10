a2f1982b523562dd71aad1e9865e0c86a5ed80ea
Zandiiee <zandilezulu13@gmail.com>
2026-06-10 08:24:23 +0200

    Error Handling added

---
+++ PA1301.js
@@ -0,0 +1,12 @@
+try{
+    console.log(divide(10, 0));
+    let num = 3/0;
+    console.log(num.toUpperCase());            
+} catch(error){
+    console.log("An error occured:", error.message);
+}
+
+let user = {
+    name:" Zandie",
+    age:23
+}
