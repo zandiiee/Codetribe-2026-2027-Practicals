3d55cdd42337f4addd39043469e00ab10ed0f3c4
Zandiiee <zandilezulu13@gmail.com>
2026-06-23 14:34:51 +0200

    PA1207

---
+++ PA1207.sql
@@ -0,0 +1,13 @@
+Create Database CustomerRecordsDB;
+use CustomerRecordsDB;
+Create Table Records(
+CustomerID Int,
+ CustomerName Varchar(200),
+ CustomerLocation Varchar(200),
+ CustomerOrder Varchar(200)
+ );
+ 
+Desc Records;
+select * from Records
+ Where CustomerName = "John"
+ And CustomerID >100;
