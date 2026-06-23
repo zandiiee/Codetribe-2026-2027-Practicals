11c96fc36f1de6ac8a8e58a001a71be273c1b478
Zandiiee <zandilezulu13@gmail.com>
2026-06-23 13:20:10 +0200

    PA1202

---
+++ PA1202.sql
@@ -0,0 +1,10 @@
+Create Database CustomerRecordsDB;
+use CustomerRecordsDB;
+Create Table Records(
+CustomerID Int,
+ CustomerName Varchar(200),
+ CustomerLocation Varchar(200),
+ CustomerOrder Varchar(200)
+ );
+ 
+Desc Records;
