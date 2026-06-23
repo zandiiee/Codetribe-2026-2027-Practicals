53115ab6e8969da4276e300b35b6f49e8868c0a5
Zandiiee <zandilezulu13@gmail.com>
2026-06-23 15:21:14 +0200

    Add files via upload
    PA1207

--- PA1207.sql
+++ PA1207.sql
@@ -7,7 +7,14 @@ CustomerID Int,
  CustomerOrder Varchar(200)
  );
  
+  
+ Insert into Records(CustomerID, CustomerName, CustomerLocation, CustomerOrder)
+ Values
+ ( 089, "Lee-Shaan", "Cape Town", "Bags"),
+ (070, "Titi","Durban", "Shoes"),
+ (055, "Tlotli","Pretoria", "Weapons");
+ 
 Desc Records;
 select * from Records
  Where CustomerName = "John"
- And CustomerID >100;
+ And CustomerID <100;
